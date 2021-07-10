73ae5bf64c605fb7792e777f45758e9dd94c03bf
softech.series <softech.series@gmail.com>
2021-07-10 13:34:34 +0700

    tambah data lagi

---
+++ kuliah/pertemuan9/pertemuan10/detail.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail</title>
+</head>
+
+<body>
+  <h2>detail mahasiswa </h2>
+  <ul>
+    <li>NPM</li>
+    <li>nama</li>
+    <li>alamat</li>
+    <li><a href="hapsu">hapus</a> | <a href="edit">edit</a></li>
+    <li><a href="latihan3.php">kembali ke daftar mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
---
+++ kuliah/pertemuan9/pertemuan10/latihan3.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'function.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa ");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>belajar menjadi backand</title>
+</head>
+
+<body>
+  <h3>Data Mahasiswa</h3>
+  <table border="1">
+    <tr>
+      <th>No</th>
+      <th>Nama</th>
+      <th>aksi</th>
+    </tr>
+    <?php $i = 1;
+    foreach ($mahasiswa as $mhs) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><?= $mhs['nama']; ?></td>
+        <td><a href="detail.php?id=<?= $mhs['id']; ?>">Lihat Detail</a>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
